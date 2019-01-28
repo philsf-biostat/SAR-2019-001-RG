9ce7c85fbb59cf4ad4ff069e0228ff230ee54a24
Felipe Figueiredo <philsf79@gmail.com>
2019-01-28 15:01:03 -0200

    obsoletos removidos

--- scripts/resultados.R
+++ scripts/resultados.R
@@ -7,9 +7,6 @@ source('scripts/res-assoc-tempo-EF.R', encoding = 'UTF-8')
 source('scripts/res-assoc-tempo-idade.R', encoding = 'UTF-8')
 source('scripts/res-assoc-tempo-nivel.R', encoding = 'UTF-8')
 
-tab.ef.t1 <- CreateTableOne(ef.colnames, data = participantes)
-tab.ef <- print(tab.ef.t1, printToggle = FALSE)
-
 tab.esportes <- rbind(
   print(tab.esportes.cirurg, exact = TRUE, printToggle = FALSE),
   print(tab.esportes.movim, exact = FALSE, printToggle = FALSE)
@@ -22,7 +19,6 @@ tab.tempo <- rbind(
 )
 
 # Pós-processamento das tabelas
-rownames(tab.ef) <- str_replace(rownames(tab.ef), '= 1 \\(%\\)', '(%)')
 rownames(tab.esportes) <- str_replace(rownames(tab.esportes), '= 1 \\(%\\)', '(%)')
 rownames(tab.tempo) <- str_replace(rownames(tab.tempo), '= 1 \\(%\\)', '(%)')
 rownames(tab.tempo) <- str_replace(rownames(tab.tempo), 'IDADE.cat', 'IDADE')
